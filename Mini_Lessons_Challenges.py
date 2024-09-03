3ec383336755c00972f2127f90c6b371125927c8
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-03 08:38:54 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,8 +1,12 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
+while True:
+    try:
+          ABQ = int(input("Please input a number! -> "))
+          break 
+    except ValueError:
+         print("Please enter in a number.")
+
 file = open("example.txt", "r")
-print(file.read(10)) # Prints out the first 10 characters
-print(file.readline()) # Prints out a line of text in the file
-print(file.readline())
-print(file.readlines()) # Returns all the lines in a file as a list with each being a line
+print(file.read(ABQ))
 file.close()
