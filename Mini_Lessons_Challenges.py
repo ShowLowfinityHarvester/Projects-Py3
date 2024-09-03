dc7afc042761c58c8677143ddc9e956b8226a9f4
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-03 09:01:14 -0700

    The first-generation Ford Focus, introduced to the U.S. market for the 2000 model year, was a compact car that marked a significant shift in Ford's approach to small car design. It was available in various body styles, including a 3-door hatchback, 4-door sedan, and 5-door wagon, catering to a wide range of buyers
    The first-generation Ford Focus, introduced to the U.S. market for the 2000 model year, was a compact car that marked a significant shift in Ford's approach to small car design. It was available in various body styles, including a 3-door hatchback, 4-door sedan, and 5-door wagon, catering to a wide range of buyers

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,7 +1,8 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-file = open("example.txt", "r")
-ReadFile = (file.read())
-ReadSplit = ReadFile.split()
-print(len(ReadSplit))
+file = open("example.txt", "a")
+file.write("I love programming!")
 file.close()
+
+file = open("example.txt", "r")
+print(file.read())
--- examples.py
+++ examples.py
@@ -1,7 +1,7 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
 # Opening, Printing, and closing a file
-file = open("example.txt", "r")
+file = open("example.txt", "r") # "r" reads the text file
 print(file.read())
 file.close()
 
@@ -13,3 +13,12 @@ print(file.readline()) # Prints out a line of text in the file
 print(file.readline())
 print(file.readlines()) # Returns all the lines in a file as a list with each being a line
 file.close()
+
+# Opening, Writing (adding text), and printing a file
+
+file = open("example.txt", "a")  # "a" Appends a line to the text file
+file.write("I love programming!")
+file.close()
+
+file = open("example.txt", "r")
+print(file.read())
