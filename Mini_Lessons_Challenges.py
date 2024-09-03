0dac937f03ee8e2c51d445fd2d5f76aa1b886231
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-03 08:17:03 -0700

    Stuffs

---
+++ Mini_Lessons_Challenges.py
@@ -0,0 +1,6 @@
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
+# Opening, Printing, and closing a file
+file = open("example.txt", "r")
+print(file.read())
+file.close()
