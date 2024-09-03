dfa93223d710457279d0ceeaf3263a49abdbe1d7
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-03 08:54:05 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,12 +1,7 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-while True:
-    try:
-          ABQ = int(input("Please input a number! -> "))
-          break 
-    except ValueError:
-         print("Please enter in a number.")
-
 file = open("example.txt", "r")
-print(file.read(ABQ))
+ReadFile = (file.read())
+ReadSplit = ReadFile.split()
+print(len(ReadSplit))
 file.close()
