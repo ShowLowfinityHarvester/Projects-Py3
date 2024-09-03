7ffc96318a606ad12805ef1e428751418c68fafe
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-03 08:28:53 -0700

    Lesson # 2

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,6 +1,8 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-# Opening, Printing, and closing a file
 file = open("example.txt", "r")
-print(file.read())
+print(file.read(10)) # Prints out the first 10 characters
+print(file.readline()) # Prints out a line of text in the file
+print(file.readline())
+print(file.readlines()) # Returns all the lines in a file as a list with each being a line
 file.close()
--- examples.py
+++ examples.py
@@ -4,3 +4,12 @@
 file = open("example.txt", "r")
 print(file.read())
 file.close()
+
+# Reading Parts of a file
+
+file = open("example.txt", "r")
+print(file.read(10)) # Prints out the first 10 characters
+print(file.readline()) # Prints out a line of text in the file
+print(file.readline())
+print(file.readlines()) # Returns all the lines in a file as a list with each being a line
+file.close()
