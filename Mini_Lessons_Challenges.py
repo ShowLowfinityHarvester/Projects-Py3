618ea20a87a182c6f0aba6d2c93f4514aecf8ffb
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-04 09:07:14 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,11 +1,6 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-while True:
-    try: 
-        rows = int(input("Enter in a number! -> "))
-        break
-    except ValueError:
-        print("Please enter in a number. Don't enter in commas")
-rowlist = [1, 2, 3, 4, 5]
-list = [[j*rows for j in rowlist] for i in range(rows)]
+weather = input("List of weather? -> ").split()
+rowlist = ["windy", "breezy", "calm"]
+list = [[(y + " " + x) for x in rowlist] for y in weather]
 print(list)
