ecc74926560a64e21907a3975efeb2196b79cf3e
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-06 09:13:46 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,10 +1,11 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-rows = 5
-cols = 3
-list = []
-for x in range(rows):
+cols = ["apple", "grape", "peach", "cinnamon", "vanilla"]
+rows = input("Input a list of fruits -> ").split()
+lists = []
+for i in rows:
     col = []
-    for y in range(cols):
-        list.append(x)
-        print(list)
+    for j in cols:
+        col.append(i + " " + j)
+    lists.append(col)
+print(lists)
