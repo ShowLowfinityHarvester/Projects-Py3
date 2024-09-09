f9b59c6b60c3fbe773279ddb5c8a3357aebfc7a9
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-09 09:19:36 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,11 +1,11 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-my_list = [[40, 45, 50], [6, 7, 8], [100, 200, 300], [50, 60, 70], [9, 0, 1]]
+my_list = [[0, 1, 2], [10, 15, 20], [100, 200, 300], [5, 6, 7]]
 cols = 3
-rows = 5
-
+rows = 4
+usrint = int(input("What number do you want this list to be multiplied by? -> "))
 for i in range(rows):
     for j in range(cols):
-        my_list[i][j] = my_list[i][j] + 3
+        my_list[i][j] = my_list[i][j] * usrint
 
 print(my_list)
