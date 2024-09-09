f54b99da20542c78d28cda2a19e9cb5fc3b0a6f2
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-09 09:35:42 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,11 +1,14 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-my_list = [[0, 1, 2], [10, 15, 20], [100, 200, 300], [5, 6, 7]]
+x = int(input("What is the first number? -> "))
+y = int(input("What is the second number? -> "))
+z = int(input("What is the third number?-> "))
+my_list = [[0, 1, x], [10, 15, y], [100, 200, 300], [5, 6, z]]
 cols = 3
 rows = 4
-usrint = int(input("What number do you want this list to be multiplied by? -> "))
+maxval = max(max(row) for row in my_list)
 for i in range(rows):
     for j in range(cols):
-        my_list[i][j] = my_list[i][j] * usrint
+        my_list[i][j] = my_list[i][j]
 
-print(my_list)
+print(maxval)
