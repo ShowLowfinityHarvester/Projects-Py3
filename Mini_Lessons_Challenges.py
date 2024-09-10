40bfeeeaa0f4a07d8b956f649dcc0c1f03774ef6
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-10 09:23:13 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,9 +1,16 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-class Hat:
-    def __init__(self, kind, color, material):
+class Fruit:
+    def __init__(self, shapes, kind, size):
+        self.shapes = shapes
         self.kind = kind
-        self.color = color
-        self.material = material
+        self.size = size
 
-myObject = Hat("ABQ Isotopes Hat", "Red", "Felt")
+F1 = Fruit("Round", "Apple", "Small")
+F2 = Fruit("Round, Long & Pointy", "Bananna", "Long")
+F3 = Fruit("Round", "Pear", "Small")
+F4 = Fruit("Round", "Kiwi", "Small")
+print(F1.kind)
+print(F2.kind)
+print(F3.kind)
+print(F4.kind)
