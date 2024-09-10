5a416f4f9d73b4a2912cb89615dd0445ed982e88
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-10 08:13:33 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,14 +1,8 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-x = int(input("What is the first number? -> "))
-y = int(input("What is the second number? -> "))
-z = int(input("What is the third number?-> "))
-my_list = [[0, 1, x], [10, 15, y], [100, 200, 300], [5, 6, z]]
-cols = 3
-rows = 4
-maxval = max(max(row) for row in my_list)
-for i in range(rows):
-    for j in range(cols):
-        my_list[i][j] = my_list[i][j]
+class dog:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
 
-print(maxval)
