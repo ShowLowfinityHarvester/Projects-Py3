b36c68010c7f97bc2f6f0afcb22bf07f5d0fda5f
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-10 08:42:09 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,8 +1,9 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-class Car:
-    def __init__(self, kind, color, year):
-        self.kind = kind
-        self.color = color
-        self.year = year
-
+class Tree:
+    def __init__(self, fruit, height, leaf, flower, seed):
+        self.fruit = fruit
+        self.height = height
+        self.leaf = leaf
+        self.flower = flower
+        self.seed = seed
