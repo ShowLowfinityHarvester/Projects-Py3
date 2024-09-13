fec3d0367daf28384fcea7b6c98cf4538dcc9c05
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-13 08:21:36 -0700

    Update examples.py

--- examples.py
+++ examples.py
@@ -109,3 +109,18 @@ stack.pop()
 print(stack)
 
 # Recursion
+
+this_list = ['panda', 'lion', 'giraffe', 'tiger', 'elephant', 'monkey', 'fish', 'snake', 'bearded dragon', 'koala']
+
+def feeding(this_list):
+    if len(this_list) == 1:
+        animal = this_list[0]
+        print(f"The {animal} has been fed")
+    else:
+        middle = len(this_list) // 2
+        first_half = this_list[:middle]
+        second_half = this_list[middle:]
+
+    feeding(first_half)
+    feeding(second_half)
+    feeding(this_list)
