d141e4208518028ea388e40ad4be0404a3308759
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-13 09:40:09 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,16 +1,18 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-this_list = ['panda', 'lion', 'giraffe', 'tiger', 'elephant', 'monkey', 'fish', 'snake', 'bearded dragon', 'koala']
+flowers = [int(n) for n in input("How many blossoms are on each tree? -> ").split()]
 
-def feeding(this_list):
-    if len(this_list) == 1:
-        animal = this_list[0]
-        print(f"The {animal} has been fed")
+def beetree(flowers):
+    if len(flowers) == 1:
+        bee = flowers[0] * 3
+        print(bee)
     else:
-        middle = len(this_list) // 2
-        first_half = this_list[:middle]
-        second_half = this_list[middle:]
+        middle = len(flowers) // 2
+        first_half = flowers[:middle]
+        second_half = flowers[middle:]
 
-    feeding(first_half)
-    feeding(second_half)
-    feeding(this_list)
+        beetree(first_half)
+        beetree(second_half)
+
+    
+beetree(flowers)
