2905c32aa0dd27a6fa59251008a7905ff089a0e8
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-13 08:21:07 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,12 +1,16 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-answer = ["apples", "steak", "potatoes", "carrots"]
-input = input("What food are we bringing to the place? -> ")
+this_list = ['panda', 'lion', 'giraffe', 'tiger', 'elephant', 'monkey', 'fish', 'snake', 'bearded dragon', 'koala']
 
-if "s" in input:
-    answer.append(input)
-    print(answer)
-else:
-    print("The input doesn't have the letter s")
+def feeding(this_list):
+    if len(this_list) == 1:
+        animal = this_list[0]
+        print(f"The {animal} has been fed")
+    else:
+        middle = len(this_list) // 2
+        first_half = this_list[:middle]
+        second_half = this_list[middle:]
 
-
+    feeding(first_half)
+    feeding(second_half)
+    feeding(this_list)
