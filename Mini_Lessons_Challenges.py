84f92e7ab531f4f435347bbfce5e3adc7bcb3416
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-13 09:48:48 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,18 +1,9 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-flowers = [int(n) for n in input("How many blossoms are on each tree? -> ").split()]
-
-def beetree(flowers):
-    if len(flowers) == 1:
-        bee = flowers[0] * 3
-        print(bee)
+n = int(input("Number -> "))
+def fact(jay):
+    if jay <= 1:
+        return 1
     else:
-        middle = len(flowers) // 2
-        first_half = flowers[:middle]
-        second_half = flowers[middle:]
-
-        beetree(first_half)
-        beetree(second_half)
-
-    
-beetree(flowers)
+        return jay * (fact(jay-1))
+print(fact(n))
