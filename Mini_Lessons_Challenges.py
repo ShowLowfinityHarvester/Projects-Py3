4cb062bcb7b7c3df55d14507f6440c60d9fc7d71
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-20 08:57:10 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,7 +1,16 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
+def selection_sort(arr):
+    for i in range(len(arr)):
+        smaller_i = i
+        for j in range(i+1, len(arr)):
+            if arr[smaller_i] > arr[j]:
+                smaller_i = j
+        if smaller_i != i:
+            arr[i], arr[smaller_i] = arr[smaller_i], arr[i]
 
-def bubbleSort(sort_list):
-    n = len(sort_list)
-    for i in range(n-1):
-        for j in range(0, n-i-1):
-            sort_list[j], sort_list[j+1] = sort_list[j+1], sort_list[j]
+arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+
+inum = int(input("Please Input a Number -> "))
+arr.append(inum)
+selection_sort(arr)
+print(arr)
