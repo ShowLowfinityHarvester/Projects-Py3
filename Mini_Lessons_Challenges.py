0062dbdb3ef2984d40c99ac8ed1383438e02f5b2
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-24 08:36:20 -0700

    Update Mini_Lessons_Challenges.py

--- Mini_Lessons_Challenges.py
+++ Mini_Lessons_Challenges.py
@@ -1,18 +1,37 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        val_to_insert = arr[i]
-        insert_spot = i
-        while insert_spot > 0 and arr[insert_spot - 1] > val_to_insert:
-            arr[insert_spot] = arr[insert_spot - 1]
-            insert_spot -= 1
-
-        arr[insert_spot] = val_to_insert
+answer = int(input("Enter a number between 0 - 100 -> "))
 
 arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+arr.append(answer)
 
-inum = int(input("Please Input a Number -> "))
-arr.append(inum)
-insertion_sort(arr)
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        Left = arr[:mid]
+        Right = arr[mid:]
+        mergeSort(Left)
+        mergeSort(Right)
+
+        i = j = k = 0
+
+        while i < len(Left) and j < len(Right):
+            if Left[i] < Right[j]:
+                arr[k] = Left[i]
+                i += 1
+            else:
+                arr[k] = Right[j]
+                j += 1
+            k += 1
+        while i < len(Left):
+            arr[k] = Left[i]
+            i += 1
+            k += 1
+
+        while j < len(Right):
+            arr[k] = Right[j]
+            j += 1
+            k += 1
+
+mergeSort(arr)
 print(arr)
